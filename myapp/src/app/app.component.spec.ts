bbf7cfda6df26c7312c4fa85497150126f9b7331
Çetin DOĞU <cetindogu@gmail.com>
2023-09-28 18:41:17 +0300

    test cases are tested :)

--- myapp/src/app/app.component.spec.ts
+++ myapp/src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have as title 'myapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('myapp');
+    expect(app.title).toEqual('My App');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('myapp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('My App app is running!');
   });
 });
